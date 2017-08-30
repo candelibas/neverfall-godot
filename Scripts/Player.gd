e35a2c747936e0a116f920b3de03559ad5be9aba
Can Delibas <candelibas96@gmail.com>
2017-08-30 05:22:18 +0300

    jumping mechanics and animations are done

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -16,11 +16,12 @@ var motion
 var powerBar = 0
 onready var powerBarProgress = preload("res://Scenes/PowerJumpBar.tscn")
 var powerBarVal
-var jumping = false
+var jumping = true # we are starting with jumping - (may be not. we can change this in future)
 
 # Animation
 var anim = "idle"
 
+var input
 
 func _ready():
 	# Get instance of powerBar and hide for default
@@ -36,7 +37,7 @@ func _ready():
 func _input(event):
 	# Aaand, let's jump!
 	if event.is_action_released("ui_select"):
-		jumping = true
+		set_process_input(false)
 		speed_y = JUMP_FORCE
 		speed_x = powerBarVal.get_value()
 		powerBarVal.set_value(0)
@@ -47,11 +48,7 @@ func _fixed_process(delta):
 	
 	# If input pressed, set powerBar for release
 	if Input.is_action_pressed("ui_select"):
-		
-		if(jumping == true):
-			print("no my lord")
-		else:
-			prepareToJump()
+		prepareToJump()
 		
 	# Set gravity
 	speed_y += delta * GRAVITY
@@ -62,6 +59,9 @@ func _fixed_process(delta):
 	motion = move(velocity)
 	
 	if is_colliding():
+		
+		set_process_input(true) # allow player to jump
+		
 		# Slide motion for realistic physics
 		var normal = get_collision_normal()
 		motion = normal.slide(motion)
@@ -81,9 +81,10 @@ func _fixed_process(delta):
 	sprite.play(anim)
 	
 func prepareToJump():
-	powerBarVal.show()
-	powerBarVal.set_value(powerBarVal.get_value() + 2)
-	anim = "hold_jump"
+	if is_processing_input():
+		powerBarVal.show()
+		powerBarVal.set_value(powerBarVal.get_value() + 2)
+		anim = "hold_jump"
 	
 #func jump(speed):
 #	velocity.y = speed
